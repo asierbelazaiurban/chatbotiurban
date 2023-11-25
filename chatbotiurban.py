e780dd7c22097cda796bef527ab0d97f596145db
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-11-25 09:55:35 +0100

    logs

--- chatbotiurban.py
+++ chatbotiurban.py
@@ -17,6 +17,23 @@ file_handler = RotatingFileHandler('logs/chatbotiurban.log', maxBytes=10240, bac
 file_handler.setFormatter(logging.Formatter(
     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
 ))
+file_handler.setLevel(logging.DEBUG)  # Cambiado a DEBUG para capturar todos los registros
+app.logger.addHandler(file_handler)
+
+app.logger.setLevel(logging.DEBUG)  # Cambiado a DEBUG
+app.logger.info('Registro de prueba inmediatamente después de la configuración de logging')
+
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+# Configuración del registro de logs
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+file_handler = RotatingFileHandler('logs/chatbotiurban.log', maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+))
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
 
