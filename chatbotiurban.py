ed3c50b0847b5412fd46fa107f98017f44f683d7
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-11-27 13:54:18 +0100

    ,

--- chatbotiurban.py
+++ chatbotiurban.py
@@ -32,35 +32,26 @@ from requests.exceptions import RequestException
 app = Flask(__name__)
 
 
-# Configuración del registro de logs
+##### Configuración del registro de logs #####
+
 if not os.path.exists('logs'):
     os.mkdir('logs')
+
+# Configura un solo manejador de archivos de registro
 file_handler = RotatingFileHandler('logs/chatbotiurban.log', maxBytes=10240, backupCount=10)
 file_handler.setFormatter(logging.Formatter(
     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
 ))
-file_handler.setLevel(logging.DEBUG)  # Cambiado a DEBUG para capturar todos los registros
+file_handler.setLevel(logging.DEBUG)  # Usa DEBUG o INFO según necesites
+
+# Añade el manejador al logger de la aplicación
 app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.DEBUG)  # Asegúrate de que este nivel sea consistente con file_handler.setLevel
 
-app.logger.setLevel(logging.DEBUG)  # Cambiado a DEBUG
-app.logger.info('Registro de prueba inmediatamente después de la configuración de logging')
-
-
-
-# Configuración del registro de logs
-if not os.path.exists('logs'):
-    os.mkdir('logs')
-file_handler = RotatingFileHandler('logs/chatbotiurban.log', maxBytes=10240, backupCount=10)
-file_handler.setFormatter(logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-))
-file_handler.setLevel(logging.INFO)
-app.logger.addHandler(file_handler)
-
-app.logger.setLevel(logging.INFO)
+# Mensaje de prueba para verificar que la configuración funciona
 app.logger.info('Inicio de la aplicación ChatbotIUrban')
 
-
+#####  #####
 
 ######## FAISS ########
 
