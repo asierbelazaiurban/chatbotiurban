12f4cbd3d74aa86ba5762f05c00a4d21c4c92e19
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-11-30 11:55:19 +0100

    uploads

--- chatbotiurban_v2.py
+++ chatbotiurban_v2.py
@@ -359,13 +359,12 @@ def upload_file():
                 app.logger.info("Nuevo archivo JSON del dataset creado")
 
             # Añadir o actualizar la entrada en el dataset
-            indice = file.filename
-            dataset_entries[indice] = {
-                "indice": indice,
+            dataset_entries[file.filename] = {
+                "indice": file.filename,
                 "url": file_path,
                 "dialogue": contenido_limpio
             }
-
+            
             # Guardar el archivo JSON actualizado
             with open(dataset_file_path, 'w', encoding='utf-8') as file:
                 json.dump(dataset_entries, file, ensure_ascii=False, indent=4)
@@ -375,13 +374,7 @@ def upload_file():
             app.logger.error(f"No se pudo procesar el archivo. Error: {e}")
             return jsonify({"respuesta": f"No se pudo procesar el archivo. Error: {e}", "codigo_error": 1})
 
-        return jsonify({"respuesta": "Archivo procesado y añadido al dataset con éxito", "codigo_error": 0})
-
-    except Exception as e:
-        app.logger.error(f"Error durante el procesamiento general. Error: {e}")
         return jsonify({"respuesta": f"Error durante el procesamiento. Error: {e}", "codigo_error": 1})
-
-
 
 @app.route('/process_urls', methods=['POST'])
 def process_urls():
