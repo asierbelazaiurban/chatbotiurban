2305718cae0b82670a20e46f1a3bbf64eb12df4e
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-05 09:59:13 +0100

    .

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,7 @@
-import unittest
+import subprocess
 
-if __name__ == '__main__':
-    tests = unittest.TestLoader().discover('path_to_test_directory', pattern='test_*.py')
-    unittest.TextTestRunner().run(tests)
+def run_tests():
+    subprocess.run(["python", "run_tests.py"], check=True)
+
+if __name__ == "__main__":
+    run_tests()
