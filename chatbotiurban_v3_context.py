18fa44745a619b82773fecd46bb1a1f37c7cafda
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-13 09:48:02 +0100

    .

--- chatbotiurban_v3_context.py
+++ chatbotiurban_v3_context.py
@@ -249,7 +249,7 @@ def mejorar_respuesta_generales_con_openai(pregunta, respuesta, new_prompt="", c
                       "proporciona el contacto: info@iurban.es.")
 
     # Construir el prompt base
-    prompt_base = f"Si hay algun tema con la codificación o caracteres saltate esa parte y sigue con el resto. {contexto_adicional}\n\nPregunta reciente: {pregunta}\nRespuesta original: {respuesta}\n--\n {new_prompt}, siempre en el idioma del contexto"
+    prompt_base = f"Si hay algun tema con la codificación o caracteres, por ejemplo (Lo siento, pero parece que hay un problema con la codificación de caracteres en tu pregunta o similar...) saltate esa parte y sigue con el resto{contexto_adicional}\n\nPregunta reciente: {pregunta}\nRespuesta original: {respuesta}\n--\n {new_prompt}, siempre en el idioma del contexto"
     app.logger.info(prompt_base)
 
     # Generar la respuesta mejorada
