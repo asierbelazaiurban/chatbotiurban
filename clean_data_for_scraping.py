edb21f2c65d10fe8fe835d29ba559df6b6b5d7c5
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-14 09:54:26 +0100

    .

---
+++ clean_data_for_scraping.py
@@ -0,0 +1,68 @@
+
+import os
+import json
+import re
+from bs4 import BeautifulSoup  
+import requests  
+
+
+
+def save_dataset(dataset_entries, chatbot_id):
+    dataset_folder = os.path.join('data', 'uploads', 'datasets', chatbot_id)
+    os.makedirs(dataset_folder, exist_ok=True)
+    dataset_file_path = os.path.join(dataset_folder, 'dataset.json')
+
+    with open(dataset_file_path, 'w') as dataset_file:
+        json.dump(dataset_entries, dataset_file, indent=4)
+
+
+def read_urls(chatbot_folder, chatbot_id):
+    urls_file_path = os.path.join(chatbot_folder, f'{chatbot_id}_urls.txt')
+    
+    try:
+        with open(urls_file_path, 'r') as file:
+            urls = file.read().splitlines()
+        return urls
+    except FileNotFoundError:
+        app.logger.error(f"Archivo de URLs no encontrado para chatbot_id {chatbot_id}")
+        return None
+
+
+def clean_and_format_text(soup):
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.decompose()
+
+    # Ignorar imágenes y videos
+    for media in soup.find_all(['img', 'video']):
+        media.decompose()
+
+    # Extraer el texto del HTML
+    text = soup.get_text()
+    # Convertir el texto a minúsculas
+    text = text.lower()
+
+    # Reemplazar saltos de línea y retornos de carro por espacios
+    text = text.replace('\n', ' ').replace('\r', ' ')
+
+    # Eliminar URLs
+    text = re.sub(r'http\S+', '', text)
+
+    # Eliminar menciones de usuario (ejemplo: @usuario)
+    text = re.sub(r'@\w+', '', text)
+
+    # Eliminar hashtags (ejemplo: #hashtag)
+    text = re.sub(r'#\w+', '', text)
+
+    # Opcional: Eliminar o reemplazar caracteres especiales, según sea necesario
+    text = re.sub(r'[^\w\s,.]', '', text)
+
+    # Eliminar caracteres unicode extraños y no imprimibles
+    text = re.sub(r'[^\x00-\x7F]+', '', text)
+
+    # Eliminar espacios múltiples
+    text = re.sub(r'\s+', ' ', text)
+
+    return text.strip()
+
+
---
+++ date_management.py
@@ -0,0 +1,105 @@
+import openai
+import os
+import requests
+import json
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+
+def configure_logger():
+    if not app.debug and not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/chatbot.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Chatbot startup')
+
+configure_logger()
+
+def get_openai_response(texto, fecha_actual):
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
+    if not openai.api_key:
+        raise ValueError("OPENAI_API_KEY is not set in environment variables")
+
+    instruccion_gpt4 = ("Tu tarea es identificar las referencias temporales en la pregunta del usuario y convertirlas a fechas específicas en formato MySQL, utilizando la fecha actual como referencia. Ejemplos de referencias temporales incluyen 'dentro de dos días', 'el finde', 'la semana que viene', 'mañana', 'en 2024', etc. La fecha actual es: " + fecha_actual.strftime("%Y-%m-%d"))
+
+    respuesta = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": instruccion_gpt4},
+            {"role": "user", "content": texto},
+        ]
+    )
+
+    respuesta_texto = respuesta.choices[0].message['content']
+    app.logger.info("Respuesta de OpenAI: %s", respuesta_texto)
+
+    # Extraer fechas del texto
+    fechas_encontradas = re.findall(r'\d{4}-\d{2}-\d{2}', respuesta_texto)
+    app.logger.info("Fechas encontradas en el texto: %s", fechas_encontradas)
+
+    return fechas_encontradas
+
+def asegurarse_el_formato_mysql(texto, fecha_actual):
+    fechas_encontradas = get_openai_response(texto, fecha_actual)
+    app.logger.info("Fechas encontradas: %s", fechas_encontradas)
+
+    fecha_inicial = fecha_final = None
+    if fechas_encontradas:
+        fecha_inicial = fechas_encontradas[0]
+        fecha_final = fechas_encontradas[1] if len(fechas_encontradas) > 1 else fecha_inicial
+
+    app.logger.info("Fecha inicial (MySQL): %s", fecha_inicial)
+    app.logger.info("Fecha final (MySQL): %s", fecha_final)
+
+    return fecha_inicial, fecha_final
+
+def obtener_eventos(pregunta, chatbot_id):
+    fecha_actual = datetime.now()
+    fecha_inicial, fecha_final = asegurarse_el_formato_mysql(pregunta, fecha_actual)
+
+    app.logger.info("Fecha inicial obtenida: %s", fecha_inicial)
+    app.logger.info("Fecha final obtenida: %s", fecha_final)
+
+    if not fecha_inicial or not fecha_final:
+        app.logger.info("No se encontraron fechas válidas en la pregunta.")
+        return "No se encontraron fechas válidas en la pregunta."
+
+    app.logger.info("ID del Chatbot utilizado: %s", chatbot_id)
+
+    url = 'https://experimental.ciceroneweb.com/api/search-event-chatbot'
+    headers = {'Content-Type': 'application/json'}
+    payload = {
+        "start": fecha_inicial,
+        "end": fecha_final,
+        "chatbot_id": chatbot_id
+    }
+
+    try:
+        app.logger.info("Enviando solicitud HTTP a: %s", url)
+        app.logger.info("Payload de la solicitud: %s", json.dumps(payload))
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response.raise_for_status()
+
+        eventos_data = response.json()
+        eventos_string = json.dumps(eventos_data.get('events', []))
+        eventos_string = eventos_string.replace('\xa0', ' ').encode('utf-8', 'ignore').decode('utf-8')
+        eventos_string = eventos_string.replace('"', '').replace('\\', '').replace('[', '').replace(']', '').replace('{', '').replace('}', '').replace(',', '. ')
+
+        app.logger.info(eventos_string)
+        return eventos_string
+
+    except requests.exceptions.RequestException as e:
+        app.logger.error("Error en la solicitud HTTP: %s", e)
+        return "Error al obtener eventos: " + str(e)
+
+
+
+
