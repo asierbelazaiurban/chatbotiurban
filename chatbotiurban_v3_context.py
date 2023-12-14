0104ce60c90e7a994b32045d7c6e9c60d619a8bf
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-14 17:24:53 +0100

    .

--- chatbotiurban_v3_context.py
+++ chatbotiurban_v3_context.py
@@ -295,7 +295,7 @@ def mejorar_respuesta_generales_con_openai(pregunta, respuesta, new_prompt="", c
         respuesta_traducida = openai.ChatCompletion.create(
             model=model_gpt if model_gpt else "gpt-4",
             messages=[
-                {"role": "system", "content": f"El idioma original es {pregunta}. Traduce, literalmente {respuesta_mejorada}, asegurate de que sea una traducción literal.  Si no hubiera que traducirla por que la: {pregunta} y :{respuesta_mejorada}, estan en el mismo idioma devuélvela tal cual, no le añadas ninguna observacion de ningun tipo ni mensaje de error. No agregues comentarios ni observaciones en ningun idioma. Solo la traducción literal o la frase repetida si es el mismo idioma"},                
+                {"role": "system", "content": f"El idioma original es el de la pregunta:  {pregunta}. Traduce, literalmente {respuesta_mejorada}, al idioma de la pregiunta. Asegurate de que sea una traducción literal.  Si no hubiera que traducirla por que la pregunta: {pregunta} y la respuesta::{respuesta_mejorada}, estan en el mismo idioma devuélvela tal cual, no le añadas ninguna observacion de ningun tipo ni mensaje de error. No agregues comentarios ni observaciones en ningun idioma. Solo la traducción literal o la frase repetida si es el mismo idioma"},                
                 {"role": "user", "content": respuesta_mejorada}
             ],
             temperature=float(temperature) if temperature else 0.7
