b1ab7c06c262b5a8c9f3f1210aa5714f23f19139
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-18 09:35:40 +0100

    .

--- chatbot_v4_fast.py
+++ chatbot_v4_fast.py
@@ -280,10 +280,34 @@ def mejorar_respuesta_generales_con_openai(pregunta, respuesta, new_prompt="", c
         improved_response = response.choices[0].message['content'].strip()
         respuesta_mejorada = improved_response
         app.logger.info("Respuesta generada con éxito.")
-        return respuesta_mejorada
+        
     except Exception as e:
         app.logger.error(f"Error al interactuar con OpenAI: {e}")
         return None
+
+
+    # Intentar traducir la respuesta mejorada
+    app.logger.info("pregunta")
+    app.logger.info(pregunta)
+    app.logger.info("respuesta_mejorada")
+    app.logger.info(respuesta_mejorada)
+    try:
+        respuesta_traducida = openai.ChatCompletion.create(
+            model=model_gpt if model_gpt else "gpt-4",
+            messages=[
+                {"role": "system", "content": f"El idioma original es el de la pregunta:  {pregunta}. Traduce, literalmente {respuesta_mejorada}, al idioma de la pregiunta. Asegurate de que sea una traducción literal.  Si no hubiera que traducirla por que la pregunta: {pregunta} y la respuesta::{respuesta_mejorada}, devuévela en el mismo idioma, no le añadas ninguna observacion de ningun tipo ni mensaje de error. No agregues comentarios ni observaciones en ningun idioma. Solo la traducción literal o la frase repetida si es el mismo idioma. Responde con menos de 75 palabras"},                
+                {"role": "user", "content": respuesta_mejorada}
+            ],
+            temperature=float(temperature) if temperature else 0.7
+        )
+        respuesta_mejorada = respuesta_traducida.choices[0].message['content'].strip()
+    except Exception as e:
+        app.logger.error(f"Error al traducir la respuesta: {e}")
+
+    app.logger.info("respuesta_mejorada final")
+    app.logger.info(respuesta_mejorada)
+    return respuesta_mejorada
+
 
 
 
