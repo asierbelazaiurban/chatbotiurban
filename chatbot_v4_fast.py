40b9a3173dc7087f06f59df3e44ada50deac3484
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-18 07:46:55 +0100

    .

--- chatbot_v4_fast.py
+++ chatbot_v4_fast.py
@@ -192,7 +192,7 @@ def mejorar_respuesta_con_openai(respuesta_original, pregunta, chatbot_id):
     )
 
     # Construir el prompt base
-    prompt_base = f"Pregunta: {pregunta}\nRespuesta: {respuesta_original}\n--\n{new_prompt}. Respondiendo siempre en el idioma del contexto"
+    prompt_base = f"Pregunta: {pregunta}\nRespuesta: {respuesta_original}\n--\n{new_prompt}. Respondiendo siempre en el idioma del contexto. Responde con no mas de 50 palabras"
 
     # Intentar generar la respuesta mejorada
     try:
@@ -207,6 +207,30 @@ def mejorar_respuesta_con_openai(respuesta_original, pregunta, chatbot_id):
     except Exception as e:
         app.logger.error(f"Error al interactuar con OpenAI: {e}")
         return None
+
+
+    # Intentar traducir la respuesta mejorada
+    app.logger.info("pregunta")
+    app.logger.info(pregunta)
+    app.logger.info("respuesta_mejorada")
+    app.logger.info(respuesta_mejorada)
+    try:
+        respuesta_traducida = openai.ChatCompletion.create(
+            model=model_gpt if model_gpt else "gpt-4",
+            messages=[
+                {"role": "system", "content": f"Responde con no mas de 50 palabras. El idioma original es el de la pregunta:  {pregunta}. Traduce, literalmente {respuesta_mejorada}, al idioma de la pregiunta. Asegurate de que sea una traducción literal.  Si no hubiera que traducirla por que la pregunta: {pregunta} y la respuesta::{respuesta_mejorada}, estan en el mismo idioma devuélvela tal cual, no le añadas ninguna observacion de ningun tipo ni mensaje de error. No agregues comentarios ni observaciones en ningun idioma. Solo la traducción literal o la frase repetida si es el mismo idioma"},                
+                {"role": "user", "content": respuesta_mejorada}
+            ],
+            temperature=float(temperature) if temperature else 0.7
+        )
+        respuesta_mejorada = respuesta_traducida.choices[0].message['content'].strip()
+    except Exception as e:
+        app.logger.error(f"Error al traducir la respuesta: {e}")
+
+    app.logger.info("respuesta_mejorada final")
+    app.logger.info(respuesta_mejorada)
+    return respuesta_mejorada
+
 
 
 def mejorar_respuesta_generales_con_openai(pregunta, respuesta, new_prompt="", contexto_adicional="", temperature="", model_gpt="", chatbot_id=""):
@@ -540,7 +564,7 @@ def encontrar_respuesta(pregunta, datos_del_dataset, vectorizer, contexto, n=1):
         if isinstance(contexto_texto, list) and all(isinstance(item, str) for item in contexto_texto):
             # Concatenar el texto para formar la respuesta, limitando a 100 palabras
             respuesta_concatenada = ' '.join(contexto_texto)
-            palabras_respuesta = respuesta_concatenada.split()[:100]
+            palabras_respuesta = respuesta_concatenada.split()[:50]
             contexto_ampliado = ' '.join(palabras_respuesta)
             return contexto_ampliado
         else:
