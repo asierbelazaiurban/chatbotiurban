1521320ec9eda5965a41663d572e7cd0ce51fdcb
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-18 09:33:49 +0100

    .

--- chatbot_v4_fast.py
+++ chatbot_v4_fast.py
@@ -197,7 +197,7 @@ def mejorar_respuesta_con_openai(respuesta_original, pregunta, chatbot_id):
     # Intentar generar la respuesta mejorada
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
+            model="gpt-4",
             messages=[
                 {"role": "system", "content": prompt_base},
                 {"role": "user", "content": respuesta_original}
@@ -264,13 +264,13 @@ def mejorar_respuesta_generales_con_openai(pregunta, respuesta, new_prompt="", c
                       "proporciona el contacto: info@iurban.es.")
 
     # Construir el prompt base
-    prompt_base = f"Si hay algun tema con la codificación o caracteres, por ejemplo (Lo siento, pero parece que hay un problema con la codificación de caracteres en tu pregunta o similar...)no te refieras  ni comentes el problema {contexto_adicional}\n\nPregunta reciente: {pregunta}\nRespuesta original: {respuesta}\n--\n {new_prompt}, siempre en el idioma del contexto"
+    prompt_base = f"El idioma original es el de la pregunta:  {pregunta}. Traduce, literalmente {respuesta}, al idioma de la pregiunta. Asegurate de que sea una traducción literal.  Si no hubiera que traducirla por que la pregunta: {pregunta} y la respuesta::{respuesta}, devuévela en el mismo idioma, no le añadas ninguna observacion de ningun tipo ni mensaje de error. No agregues comentarios ni observaciones en ningun idioma. Solo la traducción literal o la frase repetida si es el mismo idioma.Si hay algun tema con la codificación o caracteres, por ejemplo (Lo siento, pero parece que hay un problema con la codificación de caracteres en tu pregunta o similar...)no te refieras  ni comentes el problema {contexto_adicional}\n\nPregunta reciente: {pregunta}\nRespuesta original: {respuesta}\n--\n {new_prompt}, siempre en el idioma del contexto. Responde con menos de 50 palabras"
     app.logger.info(prompt_base)
 
     # Generar la respuesta mejorada
     try:
         response = openai.ChatCompletion.create(
-            model=model_gpt if model_gpt else "gpt-4-1106-preview",
+            model=model_gpt if model_gpt else "gpt-4",
             messages=[
                 {"role": "system", "content": prompt_base},
                 {"role": "user", "content": respuesta}
@@ -280,34 +280,10 @@ def mejorar_respuesta_generales_con_openai(pregunta, respuesta, new_prompt="", c
         improved_response = response.choices[0].message['content'].strip()
         respuesta_mejorada = improved_response
         app.logger.info("Respuesta generada con éxito.")
-        
+        return respuesta_mejorada
     except Exception as e:
         app.logger.error(f"Error al interactuar con OpenAI: {e}")
         return None
-
-
-    # Intentar traducir la respuesta mejorada
-    app.logger.info("pregunta")
-    app.logger.info(pregunta)
-    app.logger.info("respuesta_mejorada")
-    app.logger.info(respuesta_mejorada)
-    try:
-        respuesta_traducida = openai.ChatCompletion.create(
-            model=model_gpt if model_gpt else "gpt-4-1106-preview",
-            messages=[
-                {"role": "system", "content": f"El idioma original es el de la pregunta:  {pregunta}. Traduce, literalmente {respuesta_mejorada}, al idioma de la pregiunta. Asegurate de que sea una traducción literal.  Si no hubiera que traducirla por que la pregunta: {pregunta} y la respuesta::{respuesta_mejorada}, estan en el mismo idioma devuélvela tal cual, no le añadas ninguna observacion de ningun tipo ni mensaje de error. No agregues comentarios ni observaciones en ningun idioma. Solo la traducción literal o la frase repetida si es el mismo idioma"},                
-                {"role": "user", "content": respuesta_mejorada}
-            ],
-            temperature=float(temperature) if temperature else 0.7
-        )
-        respuesta_mejorada = respuesta_traducida.choices[0].message['content'].strip()
-    except Exception as e:
-        app.logger.error(f"Error al traducir la respuesta: {e}")
-
-    app.logger.info("respuesta_mejorada final")
-    app.logger.info(respuesta_mejorada)
-    return respuesta_mejorada
-
 
 
 
@@ -316,7 +292,7 @@ def generar_contexto_con_openai(historial):
 
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
+            model="gpt-4",
             messages=[
                 {"role": "system", "content": "Enviamos una conversación para que entiendas el contexto"},
                 {"role": "user", "content": historial}
@@ -340,7 +316,7 @@ def buscar_en_openai_relacion_con_eventos(frase):
 
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
+            model="gpt-4",
             messages=[
                 {"role": "system", "content": ""},
                 {"role": "user", "content": frase_combinada}
@@ -403,7 +379,7 @@ def identificar_saludo_despedida(frase):
     try:
         # Enviar la frase directamente a OpenAI para determinar si es un saludo o despedida
         response = openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
+            model="gpt-4",
             messages=[
                 {"role": "system", "content": "Identifica si la siguiente frase es exclusivamente un saludo o una despedida, sin información adicional o solicitudes. Responder únicamente con 'saludo', 'despedida' o 'ninguna':"},
                 {"role": "user", "content": frase}
@@ -432,7 +408,7 @@ def identificar_saludo_despedida(frase):
 
         # Realizar una segunda llamada a OpenAI para traducir la respuesta seleccionada
         traduccion_response = openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
+            model="gpt-4",
             messages=[
                 {"role": "system", "content": f"El idioma original es {frase}. Traduce, literalmente {respuesta_elegida}, asegurate de que sea una traducción literal.  Si no hubiera que traducirla por que la: {frase} y :{respuesta_elegida}, estan en el mismo idioma devuélvela tal cual, no le añadas ninguna observacion de ningun tipo ni mensaje de error. No agregues comentarios ni observaciones en ningun idioma. Solo la traducción literal o la frase repetida si es el mismo idioma"},                
                 {"role": "user", "content": respuesta_elegida}
@@ -579,7 +555,7 @@ def traducir_texto_con_openai(texto, idioma_destino):
     try:
         prompt = f"Traduce este texto al {idioma_destino}: {texto}"
         response = openai.Completion.create(
-            model="gpt-4-1106-preview",
+            model="gpt-4",
             prompt=prompt,
             max_tokens=60
         )
@@ -1074,47 +1050,6 @@ def delete_urls():
         app.logger.error(f"Error al eliminar URLs: {e}")
         return jsonify({"status": "error", "message": str(e)}), 500
 
-@app.route('/delete_dataset_entries', methods=['POST'])
-def delete_dataset_entries():
-    data = request.json
-    urls_to_delete = set(data.get('urls', []))
-    chatbot_id = data.get('chatbot_id')
-
-    if not urls_to_delete or not chatbot_id:
-        app.logger.warning("Faltan 'urls' o 'chatbot_id' en la solicitud")
-        return jsonify({"error": "Missing 'urls' or 'chatbot_id'"}), 400
-
-    BASE_DATASET_DIR = 'data/uploads/datasets'
-    dataset_file_path = os.path.join(BASE_DATASET_DIR, str(chatbot_id), 'dataset.json')
-
-    if not os.path.exists(dataset_file_path):
-        app.logger.error("Archivo del dataset no encontrado")
-        return jsonify({"status": "error", "message": "Dataset file not found"}), 404
-
-    try:
-        with open(dataset_file_path, 'r+') as file:
-            dataset = json.load(file)
-            urls_in_dataset = {entry['url'] for entry in dataset}
-            urls_not_found = urls_to_delete - urls_in_dataset
-
-            if urls_not_found:
-                app.logger.info(f"URLs no encontradas en el dataset: {urls_not_found}")
-
-            updated_dataset = [entry for entry in dataset if entry['url'] not in urls_to_delete]
-
-            file.seek(0)
-            file.truncate()
-            json.dump(updated_dataset, file, indent=4)
-
-        app.logger.info("Proceso de eliminación completado con éxito")
-        return jsonify({"status": "success", "message": "Dataset entries deleted successfully", "urls_not_found": list(urls_not_found)})
-    except json.JSONDecodeError:
-        app.logger.error("Error al leer el archivo JSON")
-        return jsonify({"status": "error", "message": "Invalid JSON format in dataset file"}), 500
-    except Exception as e:
-        app.logger.error(f"Error inesperado: {e}")
-        return jsonify({"status": "error", "message": str(e)}), 500
-
 
 
 @app.route('/pre_established_answers', methods=['POST'])
@@ -1195,7 +1130,7 @@ def change_params():
     new_prompt = data.get('new_prompt')
     chatbot_id = data.get('chatbot_id')
     temperature = data.get('temperature', '')
-    model_gpt = data.get('model_gpt', 'gpt-4-1106-preview')
+    model_gpt = data.get('model_gpt', 'gpt-4')
 
     if not new_prompt or not chatbot_id:
         app.logger.warning("Los campos 'new_prompt' y 'chatbot_id' son requeridos.")
