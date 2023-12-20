9c6d59c297a990688652fd6b97d90e4b56963dc9
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-20 08:27:46 +0100

    .

--- process_docs.py
+++ process_docs.py
@@ -50,9 +50,12 @@ def read_pdf(file_path):
     try:
         with pdfplumber.open(file_path) as pdf:
             for page in pdf.pages:
-                page_text = page.extract_text()
-                if page_text:
-                    text += page_text
+                try:
+                    page_text = page.extract_text()
+                    if page_text:
+                        text += page_text
+                except UnicodeDecodeError:
+                    text += " [Error en la decodificación de esta página] "
     except Exception as e:
         print(f"Error al procesar PDF: {e}")
         return "Error de lectura del archivo PDF"
