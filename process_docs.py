cbadebaea177cdb2a7e93f9e9f81f16aa1ed43e9
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-20 07:34:25 +0100

    .

--- process_docs.py
+++ process_docs.py
@@ -15,20 +15,35 @@ app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'csv', 'docx', 'xlsx', 'pptx'}
 
-# Funciones para leer diferentes tipos de archivos
 def read_txt(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except UnicodeDecodeError:
+        with open(file_path, 'r', encoding='latin-1') as file:
+            return file.read()
 
 def read_pdf(file_path):
     text = ""
-    with pdfplumber.open(file_path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text() or ''
+    try:
+        with pdfplumber.open(file_path) as pdf:
+            for page in pdf.pages:
+                try:
+                    page_text = page.extract_text()
+                    if page_text:
+                        text += page_text
+                except UnicodeDecodeError:
+                    # Intenta una codificación alternativa o maneja el error
+                    pass
+    except Exception as e:
+        print(f"Error al procesar PDF: {e}")
     return text
 
 def read_csv_or_xlsx(file_path, extension):
-    df = pd.read_csv(file_path) if extension == 'csv' else pd.read_excel(file_path)
+    if extension == 'csv':
+        df = pd.read_csv(file_path, encoding='utf-8')
+    else:
+        df = pd.read_excel(file_path)
     return df.to_string()
 
 def read_docx(file_path):
@@ -44,8 +59,6 @@ def read_pptx(file_path):
                 text += shape.text + "\n"
     return text
 
-
-
 def process_file(file_path, extension):
     if extension == 'txt':
         return read_txt(file_path)
@@ -59,4 +72,4 @@ def process_file(file_path, extension):
         return read_pptx(file_path)
     else:
         return None
-        
+
