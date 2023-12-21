80273e16b939ddd7bb1f1ca3d530f3e6b29c3e18
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-21 01:08:46 +0100

    .

--- main.py
+++ main.py
@@ -653,7 +653,7 @@ respuestas_por_defecto = [
 def buscar_en_respuestas_preestablecidas_nlp(pregunta_usuario, chatbot_id, umbral_similitud=0.5):
     app.logger.info("Iniciando búsqueda en respuestas preestablecidas con NLP")
 
-    modelo = SentenceTransformer('paraphrase-MiniLM-L6-v2')  # Un modelo preentrenado
+    modelo = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
     json_file_path = f'data/uploads/pre_established_answers/{chatbot_id}/pre_established_answers.json'
 
     if not os.path.exists(json_file_path):
@@ -663,38 +663,29 @@ def buscar_en_respuestas_preestablecidas_nlp(pregunta_usuario, chatbot_id, umbra
     with open(json_file_path, 'r', encoding='utf-8') as json_file:
         preguntas_respuestas = json.load(json_file)
 
-    # Crear una lista de todas las palabras clave
     palabras_clave = [entry["palabras_clave"] for entry in preguntas_respuestas.values()]
     palabras_clave_flat = [' '.join(palabras) for palabras in palabras_clave]
 
-    # Calcular los embeddings para las palabras clave y la pregunta del usuario
     embeddings_palabras_clave = modelo.encode(palabras_clave_flat, convert_to_tensor=True)
     embedding_pregunta_usuario = modelo.encode(pregunta_usuario, convert_to_tensor=True)
 
-    # Calcular la similitud semántica
     similitudes = util.pytorch_cos_sim(embedding_pregunta_usuario, embeddings_palabras_clave)[0]
 
-    # Encontrar la mejor coincidencia si supera el umbral
     mejor_coincidencia = similitudes.argmax()
     max_similitud = similitudes[mejor_coincidencia].item()
 
     if max_similitud >= umbral_similitud:
         respuesta_mejor_coincidencia = list(preguntas_respuestas.values())[mejor_coincidencia]["respuesta"]
-
-        app.logger.info("respuesta_mejor_coincidencia")
-        app.logger.info(respuesta_mejor_coincidencia)
-
-        sys.exit()
-
+        
         if comprobar_coherencia_gpt(pregunta_usuario, respuesta_mejor_coincidencia):
             app.logger.info(f"Respuesta encontrada con una similitud de {max_similitud} y coherencia verificada")
             return respuesta_mejor_coincidencia
         else:
             app.logger.info("La respuesta no es coherente según GPT")
-            return  False
+            return False
     else:
         app.logger.info("No se encontró una coincidencia adecuada")
-        return  False
+        return False
 
 
 def comprobar_coherencia_gpt(pregunta, respuesta):
