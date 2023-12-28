466f5199330e1c7cb09c50cc2ef8c90adafba8d3
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-28 18:13:59 +0100

    .

--- main.py
+++ main.py
@@ -583,51 +583,34 @@ def buscar_con_bert_en_elasticsearch(query, indice_elasticsearch, max_size=25):
         return False
 
 
-def encontrar_respuesta(ultima_pregunta, datos_del_dataset, chatbot_id, contexto=""):
-    app.logger.info("Iniciando función encontrar_respuesta.")
-    app.logger.info(f"Pregunta recibida: {ultima_pregunta}")
-    app.logger.info(f"Chatbot ID: {chatbot_id}")
-    app.logger.info(f"Contexto adicional: {contexto}")
-
-
+def encontrar_respuesta(ultima_pregunta, datos_del_dataset, chatbot_id, contexto_adicional=""):
     if not ultima_pregunta or not datos_del_dataset or not chatbot_id:
-        app.logger.info("Falta información importante: pregunta, dataset o chatbot_id")
         return False
 
-    app.logger.info("Preprocesando texto combinado de pregunta y contexto.")
-    texto_completo = f"{ultima_pregunta} {contexto}".strip()
+    # Preprocesamiento de la pregunta y el contexto adicional
+    texto_completo = f"{ultima_pregunta} {contexto_adicional}".strip()
     texto_procesado = preprocess_text(texto_completo)
 
-    app.logger.info(f"Texto procesado para búsqueda: {texto_procesado}")
-    app.logger.info("Realizando búsqueda semántica en Elasticsearch.")
-    resultados_elasticsearch = buscar_con_bert_en_elasticsearch(
-        texto_procesado, 
-        INDICE_ELASTICSEARCH, 
-        max_size=25
-    )
-
+    # Búsqueda en Elasticsearch
+    resultados_elasticsearch = buscar_con_bert_en_elasticsearch(texto_procesado, INDICE_ELASTICSEARCH, max_size=25)
     if not resultados_elasticsearch:
-        app.logger.info("No se encontraron resultados relevantes en Elasticsearch.")
         return "No se encontraron resultados relevantes."
 
-    app.logger.info("Creando contexto para GPT a partir de los resultados de Elasticsearch.")
-    contexto_para_gpt = " ".join([
-        resultado['_source'].get('text', '') 
-        for resultado in resultados_elasticsearch[:5] 
-    ])
+    # Procesamiento de los resultados de Elasticsearch
+    texto_completo_elasticsearch = " ".join([resultado['_source'].get('text', '') for resultado in resultados_elasticsearch[:5]])
+    resumen = generar_resumen_con_bert(texto_completo_elasticsearch)
+    ideas_clave = extraer_ideas_clave_con_bert(texto_completo_elasticsearch)
 
-    if not contexto_para_gpt.strip():
-        app.logger.info("No se pudo generar contexto a partir de los resultados de Elasticsearch.")
-        return "No se pudo generar contexto a partir de los resultados de Elasticsearch."
+    # Crear contexto para GPT incluyendo el contexto adicional y el generado
+    contexto_para_gpt = f"Contexto adicional: {contexto_adicional}. Resumen: {resumen}. Ideas clave: {', '.join(ideas_clave)}."
 
-    app.logger.info("Manejando prompt personalizado si existe.")
+    # Construcción del prompt final
+    prompt_personalizado = None
     try:
         with open(os.path.join(BASE_PROMPTS_DIR, str(chatbot_id), 'prompt.txt'), 'r') as file:
             prompt_personalizado = file.read()
-            app.logger.info("Prompt personalizado cargado con éxito.")
     except Exception as e:
-        app.logger.error(f"Error al cargar prompt personalizado: {e}")
-        prompt_personalizado = None
+        pass  # Manejar el error como consideres apropiado
 
     final_prompt = prompt_personalizado if prompt_personalizado else (
         "Somos una agencia de turismo especializada. Mejora la respuesta siguiendo estas instrucciones claras: "
@@ -637,25 +620,27 @@ def encontrar_respuesta(ultima_pregunta, datos_del_dataset, chatbot_id, contexto
         "4. Encuentra la mejor respuesta en relación a la pregunta que te llega "
         "Recuerda, la respuesta debe ser concisa y no exceder las 100 palabras."
     )
-    app.logger.info("Prompt final generado.")
 
+    # Combina el contexto para GPT con el prompt base
     prompt_base = f"Contexto: {contexto_para_gpt}\nPregunta: {ultima_pregunta}\nRespuesta:"
-    app.logger.info("Generando respuesta utilizando GPT-4-1106-preview.")
-    
+
     try:
         response = openai.ChatCompletion.create(
             model="gpt-4-1106-preview",
             messages=[
-                {"role": "system", "content": prompt_base},
+                {"role": "system", "content": prompt_base + final_prompt},
                 {"role": "user", "content": ""}
             ]
         )
         respuesta = response.choices[0].message['content'].strip()
-        app.logger.info("Respuesta generada con éxito.")
         return respuesta
     except Exception as e:
-        app.logger.error(f"Error al generar respuesta con GPT-4-1106-preview: {e}")
         return "Error al generar respuesta."
+
+# Ejemplo de uso
+respuesta = encontrar_respuesta("¿Cuál es el mejor lugar para visitar en París?", datos_del_dataset, "chatbot_id_ejemplo", "Contexto de conversación anterior")
+print(respuesta)
+
 
 
 
@@ -680,34 +665,21 @@ def generar_resumen_con_bert(texto):
     oraciones = sent_tokenize(texto)
     embeddings = np.array([obtener_embedding_bert(oracion) for oracion in oraciones])
 
-    # Calcular la similitud de cada oración con el texto completo
     similitudes = cosine_similarity(embeddings, embeddings.mean(axis=0).reshape(1, -1))
-
-    # Seleccionar las oraciones más representativas
-    indices_importantes = np.argsort(similitudes, axis=0)[::-1][:5]  # Ejemplo: seleccionar top 5
+    indices_importantes = np.argsort(similitudes, axis=0)[::-1][:5]
     resumen = ' '.join([oraciones[i] for i in indices_importantes.flatten()])
 
     return resumen
 
 def extraer_ideas_clave_con_bert(texto):
-    # Obtener entidades nombradas
     entidades = nlp_ner(texto)
-
-    # Crear una lista para almacenar las ideas clave
     ideas_clave = set()
 
-    # Filtrar y agregar entidades relevantes a las ideas clave
     for entidad in entidades:
-        if entidad['entity'] in ['B-ORG', 'B-PER', 'B-LOC']:  # Ejemplo de tipos de entidades
+        if entidad['entity'] in ['B-ORG', 'B-PER', 'B-LOC']:
             ideas_clave.add(entidad['word'])
 
     return list(ideas_clave)
-
-
-
-
-from elasticsearch import Elasticsearch, helpers
-
 
 
 # Fine-tuning de BERT
