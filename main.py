c9d69529a3896e79aff0e121d3d03cd5ec624c9b
asierbelazaiurban <151062824+asierbelazaiurban@users.noreply.github.com>
2023-12-28 18:39:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -538,6 +538,8 @@ def dividir_texto_largo(texto, max_longitud=512):
 """
 
 def obtener_embedding_bert(oracion):
+    model = BertModel.from_pretrained(BASE_BERT_DIR)
+    inputs = tokenizer.encode_plus(oracion, return_tensors="pt", max_length=512, truncation=True)
     # Codificar la oración usando el tokenizer
     inputs = tokenizer(oracion, return_tensors='pt', truncation=True, max_length=512)
 
@@ -675,16 +677,17 @@ def load_and_preprocess_data(file_path):
     return processed_data
 
 
-def generar_resumen_con_bert(texto, model, tokenizer):
+
+def generar_resumen_con_bert(texto):
     oraciones = sent_tokenize(texto)
-    embeddings = [obtener_embedding_bert(oracion, model, tokenizer, max_length=128) for oracion in oraciones]
+    embeddings = np.array([obtener_embedding_bert(oracion) for oracion in oraciones])
 
-    # Asegúrate de que todos los embeddings tengan la misma longitud
-    # y combínalos en un array bidimensional
-    embeddings_array = np.vstack(embeddings)
+    # Asegúrate de que cada embedding sea un array de una dimensión.
+    # Puedes usar np.mean, np.flatten, np.squeeze, o una técnica similar según tu caso.
+    embeddings = np.array([embedding.flatten() for embedding in embeddings])
 
-    # Ahora puedes usar cosine_similarity
-    similitudes = cosine_similarity(embeddings_array, embeddings_array.mean(axis=0).reshape(1, -1))
+    # Ahora embeddings es un array 2D: número de oraciones x tamaño del embedding
+    similitudes = cosine_similarity(embeddings, embeddings.mean(axis=0).reshape(1, -1))
     indices_importantes = np.argsort(similitudes, axis=0)[::-1][:5]
     resumen = ' '.join([oraciones[i] for i in indices_importantes.flatten()])
     return resumen
